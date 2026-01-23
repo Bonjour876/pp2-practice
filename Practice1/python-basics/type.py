d0f9e3d9a214b4e1c61dfecef1ef6e712c1dca0b
Tamerlan Zhylkybay <tamerlanzhylkybay@gmail.com>
2026-01-23 12:31:50 +0500

    Add full types of python

---
+++ Practice1/python-basics/type.py
@@ -0,0 +1,31 @@
+x1 = "Hello World"	#str	
+x2 = 20	#int	
+x3 = 20.5	#float	
+x4 = 1j	#complex	
+x5 = ["apple", "banana", "cherry"]	#list	
+x6 = ("apple", "banana", "cherry")	#tuple	
+x7 = range(6)	#range	
+x8 = {"name" : "John", "age" : 36}	#dict	
+x9 = {"apple", "banana", "cherry"}	#set	
+x10 = frozenset({"apple", "banana", "cherry"})	#frozenset	
+x11 = True	#bool	
+x12 = b"Hello" #bytes	
+x13 = bytearray(5)	#bytearray	
+x14 = memoryview(bytes(5))	#memoryview	
+x15 = None
+
+print(x1, " this is str type")
+print(x2, " this is int type")
+print(x3, " this is float type")
+print(x4, " this is complex type")
+print(x5, " this is list type")
+print(x6, " this is tuple type")
+print(x7, " this is range type")
+print(x8, " this is dict type")
+print(x9, " this is set type")
+print(x10, " this is frozenset type")
+print(x11, " this is bool type")
+print(x12, " this is bytes type")
+print(x13, " this is bytearray type")
+print(x14, " this is memoryview type")
+print(x15, " this is NoneType type")
